76c4138bd3f450d9fa1aad509bc980d3eacf4d3a
nonomu <noammualmi@gmail.com>
2019-10-03 14:05:21 +0300

    add frog font +on click delete

---
+++ main.js
@@ -0,0 +1,6 @@
+ $("#Gamezone").append(`<span class="frog"> <i class="fas fa-frog"></i> </span>`)
+ $("#Gamezone").append(`<span class="frog"> <i class="fas fa-frog"></i> </span>`)
+
+$("#Gamezone").on("click", ".frog", function () {
+    $(this).remove()
+})
