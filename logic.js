a2a095940f4aac9ae419a96be4769f96f5f639c9
nonomu <noammualmi@gmail.com>
2019-10-03 14:14:58 +0300

    add logic CatchTheFrog Module +Levelup() +GetGameNum()+ Variables in object

---
+++ logic.js
@@ -0,0 +1,11 @@
+const CatchTheFrog = function () {
+    let GameNum = {
+        numoffrogs: 1,
+        levelnum: 1
+    }
+    const Levelup = function () {
+        numoffrogs += 1
+        levelnum += 1
+    }
+    const getGamenum= () => GameNum
+}
