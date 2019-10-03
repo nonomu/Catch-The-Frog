4f97d40f84bea377218b0530449d2396e30203e9
nonomu <noammualmi@gmail.com>
2019-10-03 19:05:48 +0300

    add logic functions (addfrog + delete +frogs object + return desire functions/variables

--- logic.js
+++ logic.js
@@ -1,20 +1,57 @@
 const CatchTheFrog = function () {
-    let GameNum = {
-        numoffrogs: 1,
-        levelnum: 1,
-        time:1000
-    }
+    let numoffrogs= 1
+    let levelnum= 1
+    let time=1000
+    let frogs =[ 
+            {
+                id: "p1",
+                position={
+                    top: "20vh",
+                    left: "30vh"
+                }
+            },
+            {
+                id: "p2",
+                position={
+                    top: "10vh",
+                    left: "50vh"
+                }
+            }
+        ]
     const Levelup = function () {
         numoffrogs += 1
         levelnum += 1
         time+=1000
     }
-    const getGamenum = () => GameNum
-
+    const deletefrogs= function (removefrogid) {
+        for(let i=0;i<frogs.length;i++)
+    {
+        if(frogs[i].id === removefrogid)
+        {
+            frogs.splice(i,1)
+        }
+    }
+        
+    }
+    const getfrogs = function () {
+        return frogs
+    }
+    const addfrog= function () {
+        let position1=Math.random()*80+"vh"
+        let position2=Math.random()*80+"vh"
+        let newfrog={
+            id:"p" + ++postidCounter,
+            position={top:position1, left:position2}
+        }
+        frogs.push(newfrog)
+    }
     return {
-        GameNum: GameNum,
+        numoffrogs:numoffrogs,
+        levelnum:levelnum,
+        time:time,
+        frogs:frogs,
         Levelup:Levelup,
-        getGamenum:getGamenum
-
+        getfrogs:getfrogs,
+        addfrog:addfrog
     }
 }
--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+const catchTheFrog = CatchTheFrog()
+const renderer = Renderer()
+
+renderer.renderFrogs(catchTheFrog.getGamenum())
+
  $("#Gamezone").append(`<span class="frog"> <i class="fas fa-frog"></i> </span>`)
  $("#Gamezone").append(`<span class="frog"> <i class="fas fa-frog"></i> </span>`)
 
